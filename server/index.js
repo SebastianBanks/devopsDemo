a5d3a27bcb088def4fc723c143ff0031edff62c7
Sebastian Banks <Sebastian@thebanksfamily.net>
2022-02-24 12:36:31 -0700

    added rollbar

--- server/index.js
+++ server/index.js
@@ -2,8 +2,8 @@ const express = require("express")
 const path = require("path")
 
 // include and initialize the rollbar library with your access token
-var Rollbar = require('rollbar')
-var rollbar = new Rollbar({
+const Rollbar = require('rollbar')
+const rollbar = new Rollbar({
   accessToken: '2e20cff43db244f48ab08bf32fa2bb08',
   captureUncaught: true,
   captureUnhandledRejections: true,
@@ -18,6 +18,18 @@ app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "../public/index.html"))
 })
 
+let students = []
+
+app.post('/api/student', (req, res)=>{
+    let {name} = req.body
+    name = name.trim()
+    rollbar.log(`Student was added successfully`, {author: "Seb", type: "manual"})
+    students.push(name)
+
+    res.status(200).send(students)
+})
+
+app.use(rollbar.errorHandler())
 app.use(express.static(path.join(__dirname, "../public")))
 
 const port = process.env.PORT || 4545
